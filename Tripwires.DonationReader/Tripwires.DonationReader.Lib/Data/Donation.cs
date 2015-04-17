9a0191d388f67ab5eb398b39f174c09ec8e4bab1
tripwires <michel.brouckaert@gmail.com>
2015-04-17 22:39:14 +0200

    initial start

---
+++ Tripwires.DonationReader/Tripwires.DonationReader.Lib/Data/Donation.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace Tripwires.DonationReader.Lib.Data
+{
+    public class Donation
+    {
+        /**
+         * {
+         "donations":[
+          {
+           "username":"Donation-Tracker",
+           "note":"Your message goes here!",
+           "timestamp":"1407240045",
+           "amount":"75.00",
+           "currency":"USD",
+           "currency_symbol":"$",
+           "transaction_id":"DT-F0PGNGB3O",
+           "paypal_email":"payment@donation-tracker.com"
+          }
+         ],
+         "api_check":"1"
+        }*/
+        private string username;
+        private string note;
+        private string timestamp;
+        private string currency;
+        private string currency_symbol;
+        private string transaction_id;
+        private string paypal_email;
+
+        [JsonProperty("username")]
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        [JsonProperty("note")]
+        public string Note
+        {
+            get { return note; }
+            set { note = value; }
+        }
+        [JsonProperty("timestamp")]
+        public string Timestamp
+        {
+            get { return timestamp; }
+            set { timestamp = value; }
+        }
+        [JsonProperty("currency")]
+        public string Currency
+        {
+            get { return currency; }
+            set { currency = value; }
+        }
+        [JsonProperty("currency_symbol")]
+        public string CurrencySymbol
+        {
+            get { return currency_symbol; }
+            set { currency_symbol = value; }
+        }
+        [JsonProperty("transaction_id")]
+        public string TransactionId
+        {
+            get { return transaction_id; }
+            set { transaction_id = value; }
+        }
+        [JsonProperty("paypal_email")]
+        public string PaypalEmail
+        {
+            get { return paypal_email; }
+            set { paypal_email = value; }
+        }
+
+    }
+}
---
+++ Tripwires.DonationReader/Tripwires.DonationReader.Lib/Data/DonationList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tripwires.DonationReader.Lib.Data
+{
+    class DonationList:List<Donation>
+    {
+    }
+}
