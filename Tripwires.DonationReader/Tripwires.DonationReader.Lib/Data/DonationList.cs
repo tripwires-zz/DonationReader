b7ddc95ff1a71e3cf432d0bef5ff15bee2194f0f
tripwires <michel.brouckaert@gmail.com>
2015-05-03 16:58:58 +0200

    added general features

--- Tripwires.DonationReader/Tripwires.DonationReader.Lib/Data/DonationList.cs
+++ Tripwires.DonationReader/Tripwires.DonationReader.Lib/Data/DonationList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,19 @@ namespace Tripwires.DonationReader.Lib.Data
 {
     class DonationList:List<Donation>
     {
+        private List<Donation> donations;
+        [JsonProperty("donations")]
+        public List<Donation> Donations
+        {
+            get { return donations; }
+            set { donations = value; }
+        }
+        private bool api_check;
+        [JsonProperty("api_check")]
+        public bool Api_check
+        {
+            get { return api_check; }
+            set { api_check = value; }
+        }
     }
 }
